9ab934ff3c169481460d08a2ed2250da104ad620
llh4github <lilinhong_coding@foxmail.com>
2026-03-06 16:51:23 +0800

    feat: 添加 Unocss 支持并移除 Tailwind CSS
    添加 Unocss 及其相关预设，配置 UnoCSS 主题颜色，并在 Vite 中启用 Unocss 插件
    移除 Tailwind CSS 相关导入

---
+++ uno.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'unocss'
+import presetUno from '@unocss/preset-uno'
+import presetAttributify from '@unocss/preset-attributify'
+import presetIcons from '@unocss/preset-icons'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+  ],
+  theme: {
+    colors: {
+      primary: '#DF7623',
+    },
+  },
+})
